f84ef561913c43a598dfdecd8fa313d554bd480f
mesay-tadesse <mesayt120@gmail.com>
2026-07-01 20:28:09 +0300

    configure enrollment relationships and delete restrictive  behavior

--- Data/Configuration/EnrollmentConfiguration.cs
+++ Data/Configuration/EnrollmentConfiguration.cs
@@ -26,9 +26,10 @@ public class EnrollmentConfiguration : IEntityTypeConfiguration<Enrollment>
         builder.HasOne(e => e.Student)
             .WithMany(s => s.Enrollments)
             .HasForeignKey(e => e.StudentId);
-
+            .OnDelete(DeleteBehavior.Restrict);
         builder.HasOne(e => e.Course)
             .WithMany(c => c.Enrollments)
             .HasForeignKey(e => e.CourseId);
+            .OnDelete(DeleteBehavior.Restrict);            
     }
 }
