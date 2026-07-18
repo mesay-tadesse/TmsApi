71cadda19fdf580dad696cd8c96a6d18b2cdade1
mesay-tadesse <mesayt120@gmail.com>
2026-07-18 21:36:05 +0300

    feat: configure API versioning and multi-version OpenAPI support

--- Program.cs
+++ Program.cs
@@ -7,9 +7,34 @@ using TmsApi.Entities;
 using TmsApi.Dtos;
 using TmsApi.Services;
 using TmsApi.Filters;
-
+using Asp.Versioning; 
 var builder = WebApplication.CreateBuilder(args);
 
+
+builder.Services.AddOpenApi("v1", options =>
+{
+    options.ShouldInclude = description =>
+        description.GroupName == "v1";
+});
+
+builder.Services.AddOpenApi("v2", options =>
+{
+    options.ShouldInclude = description =>
+        description.GroupName == "v2";
+});
+
+builder.Services.AddApiVersioning(options =>
+{
+    options.DefaultApiVersion = new ApiVersion(1, 0);
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.ReportApiVersions = true;
+    options.ApiVersionReader = new UrlSegmentApiVersionReader();
+})
+.AddApiExplorer(options =>
+{
+    options.GroupNameFormat = "'v'VVV";
+    options.SubstituteApiVersionInUrl = true;
+});
 
 builder.Services.AddDbContext<TmsDbContext>(
     options => options.UseNpgsql(
@@ -72,7 +97,20 @@ app.UseAuthorization();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-    app.MapScalarApiReference();
+    // app.MapScalarApiReference();
+    app.MapScalarApiReference(options =>
+    {
+        options.WithTitle("TMS API Reference")
+            .WithTheme(ScalarTheme.DeepSpace)
+            .WithDefaultHttpClient(
+                ScalarTarget.CSharp,
+                ScalarClient.HttpClient);
+        // Tell Scalar to pull both documents into its sidebar dropdown
+        options
+            .AddDocument("v1", "API Version 1.0")
+            .AddDocument("v2", "API Version 2.0");
+    });
+
     
     using var scope = app.Services.CreateScope();
 
